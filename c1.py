6d7a48f0c7e32c25a993a65addb3cbd8dc0723a7
Karlygach <81988198+jumabekova06@users.noreply.github.com>
2021-06-22 02:31:33 +0600

    Update c1.py
    change

--- c1.py
+++ c1.py
@@ -1,15 +1,20 @@
-from random import randint
 
-m = int (input('Введите M : '))
-n = int (input('Введите N : '))
+from random import randint
+import csv
+
+m = int(input('Введите M : '))
+n = int(input('Введите N : '))
 
 a = [[randint(1, 10) for j in range(m)] for i in range(n)]
 
-print ('Сохранить файл? : ')
+print('Сохранить файл? : ')
 t = str(input('1) Да 0) Нет : '))
 if t == '1':
     s = str(input('Введите название файла : '))
-    open_file = open(f'/home/username/Рабочий стол/{s}.csv', 'w')
-    open_file.write(f'{a}')
+    open_file = open(f'/home/user/Рабочий стол/{s}.csv', 'w')
+    writer = csv.writer(open_file)
+    writer.writerows(a)
+    open_file.close()
+    # open_file.write(f'{a}')
 else:
-    print (a)
+    print(a)
